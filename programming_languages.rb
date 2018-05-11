32cc8e0626121755a7455e99337b68edf3677792
malorie16 <malorie18@gmail.com>
2018-05-11 15:50:57 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -10,10 +10,10 @@ def reformat_languages(languages)
     new_hash[lang] = val
   end
   new_hash.each do |lang, val|
-    if val == {:type => "interpreted"}
-      new_hash[lang][:style] = [:oo]
-    elsif lang == :javascript
+    if lang == :javascript && val == {:type => "interpreted"}
       new_hash[lang][:style] = [:oo, :functional]
+    elsif val == {:type => "interpreted"}
+      new_hash[lang][:style] = [:oo]
     end
     if val == {:type => "compiled"}
       new_hash[lang][:style] = [:functional]
