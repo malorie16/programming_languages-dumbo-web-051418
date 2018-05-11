940404fc2e29d062536722ff69b5bbb6d375150b
malorie16 <malorie18@gmail.com>
2018-05-11 15:52:04 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,25 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages[:oo].each do |lang, val|
+    new_hash[lang] = val
+  end
+  languages[:functional].each do |lang, val|
+    new_hash[lang] = val
+  end
+  new_hash.each do |lang, val|
+    if lang == :javascript && val == {:type => "interpreted"}
+      new_hash[lang][:style] = [:oo, :functional]
+    elsif val == {:type => "interpreted"}
+      new_hash[lang][:style] = [:oo]
+    end
+    if lang == :java && val == {:type => "compiled"}
+      new_hash[lang][:style] = [:oo]
+    elsif val == {:type => "compiled"}
+      new_hash[lang][:style] = [:functional]
+    end
+  end
+  #binding.pry
 end
